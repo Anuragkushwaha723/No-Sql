3fcac8c901cb2a66d06ea24656eaeb69f38a19c7
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-07-13 19:35:38 +0530

    Second commit

--- util/database.js
+++ util/database.js
@@ -2,16 +2,16 @@ const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 let _db;
 const mongoConnect = (callback) => {
-  MongoClient.connect('mongodb+srv://anuragkushwaha723:0m4GHptO5JE2MVWm@cluster0.lnt4rhp.mongodb.net/?retryWrites=true&w=majority')
+  
     .then(client => {
-      console.log('Connected!');
-      _db = client.db();
-      callback();
-    })
-    .catch(err => {
-      console.log(err);
-      throw err;
-    });
+  console.log('Connected!');
+  _db = client.db();
+  callback();
+})
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
 }
 const getDb = () => {
   if (_db) {
