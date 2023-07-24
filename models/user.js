d3617bacd29f524e97045755e65da000799e011e
Anurag Kushwaha <anuragkushwaha723@gmail.com>
2023-07-24 16:27:46 +0530

    delete cart items

--- models/user.js
+++ models/user.js
@@ -35,6 +35,15 @@ userSchema.methods.addToCart = function (product) {
     this.cart = updatedCarts;
     return this.save();
 }
+
+userSchema.methods.removeFromCart = function (productId) {
+    const updatedCartItems = this.cart.items.filter(item => {
+        return item.productId.toString() !== productId.toString();
+    });
+    this.cart.items = updatedCartItems;
+    return this.save();
+}
+
 module.exports = mongoose.model('User', userSchema);
 
 
